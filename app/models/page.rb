b44937efc40b31ec188730477f4dada22fd7bdb0
Tyler Copple <j.tylercopple@gmail.com>
2012-06-06 22:17:56 -0400

    Removing description from page, updating the validations, and simplifying some logic.

--- app/models/page.rb
+++ app/models/page.rb
@@ -3,21 +3,18 @@ class Page
   include Mongoid::Timestamps
 
   field :title, type: String
-  validates_presence_of :title
-  validates_length_of :title, allow_blank: false, minimum: 4
+  validates :title, presence: true,
+                    length: { allow_blank: false, minimum: 4 }
 
   field :permalink, type: String
-  validates_presence_of :permalink
-  validates_uniqueness_of :permalink, case_sensitive: false
-  validates_length_of :permalink, allow_blank: false, minimum: 4
+  validates :permalink, presence: true,
+                        uniqueness: { case_sensitive: false },
+                        length: { allow_blank: false, minimum: 4 }
 
-  field :description, type: String
-
+  field :link, type: String, :default => nil
   field :parent, type: String
   field :visible, type: Boolean
   field :order, type: Integer
-
-  field :link, type: String, :default => nil
 
   field :markup, type: Boolean, :default => true
   field :markup_content, type: String
@@ -26,13 +23,7 @@ class Page
   key :permalink
 
   def source
-    ret = if self.markup
-            self.markup_content
-          else
-            self.html_content
-          end
-
-    ret || ""
+    (self.markup ? self.markup_content : self.html_content) || ""
   end
 
   def source= content
@@ -50,5 +41,4 @@ class Page
         .collect{|p| [p.title, p.title]}
         .unshift(["None", nil])
   end
-
 end

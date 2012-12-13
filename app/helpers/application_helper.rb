be9d1d3675711dbf344ec909bbbb53c6d87b0453
Tyler Copple <j.tylercopple@gmail.com>
2012-12-13 10:54:40 -0500

    CSS & Javascript files will not require explicit loading. The only auto loading done is for files matching the controller, action namespace.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -49,4 +49,32 @@ module ApplicationHelper
   def add_new_button(link_path, content)
     render :partial => 'shared/add_new_button', :locals => { path: link_path, content: content }
   end
+
+  def load_stylesheets
+    stylesheets = ["application"]
+
+    if Storefront::Application::assets.find_asset("#{File.join(controller_name, controller_name)}.css")
+      stylesheets << File.join(controller_name, controller_name) #controller specific manifest
+    end
+
+    if Storefront::Application::assets.find_asset("#{File.join(controller_name, action_name)}.css")
+      stylesheets << File.join(controller_name, action_name) #specific styles for action
+    end
+
+    stylesheets
+  end
+
+  def load_javascripts
+    javascripts = ["application"]
+
+    if Citychurch::Application::assets.find_asset("#{File.join(controller_name, controller_name)}.js")
+      javascripts << File.join(controller_name, controller_name) #controller specific manifest
+    end
+
+    if Citychurch::Application::assets.find_asset("#{File.join(controller_name, action_name)}.js")
+      javascripts << File.join(controller_name, action_name) #specific styles for action
+    end
+
+    javascripts
+  end
 end

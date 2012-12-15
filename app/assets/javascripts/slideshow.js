0694ff1203724aaac1ac781e3f13036b00e99b0a
Tyler Copple <j.tylercopple@gmail.com>
2012-12-15 03:11:03 +0000

    Adding pager navigation to front page banners.

--- app/assets/javascripts/slideshow.js
+++ app/assets/javascripts/slideshow.js
@@ -1,5 +1,13 @@
 var slideshow_interval_id = null;
 
 $(document).ready( function() {
-  $('ul#banners').cycle();
+  $('ul#banners').cycle({
+      pager: '#pagers',
+      slideExpr: 'img',
+
+      pagerAnchorBuilder: function(idx, slide) {
+        var src = $(slide.innerHTML).attr("src");
+        return '<a href="#"></a>';
+      }
+  });
 });

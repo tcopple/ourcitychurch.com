7d2492e865744fa80846ef577d728378b8f0a2de
Tyler Copple <j.tylercopple@gmail.com>
2013-02-03 16:58:59 -0500

    Adding proper output, stylings, and javascript controls to community groups page.

--- src/app/assets/javascripts/community_groups/community_groups.maps.js
+++ src/app/assets/javascripts/community_groups/community_groups.maps.js
@@ -1,16 +1,30 @@
 $(document).ready(function() {
   Gmaps.map.callback = function() {
     google.maps.event.addListenerOnce(Gmaps.map.serviceObject, 'idle', function() {
-      // $.each(Gmaps.map.markers, function(index, marker) {
       for(var i = 0; i < Gmaps.map.markers.length; ++i) {
         var mark = Gmaps.map.markers[i];
         var m = mark.serviceObject;
+
         m.marker_id = mark.id;
+
         google.maps.event.addListener(m, 'click', function() {
-          Gmaps.map.serviceObject.setCenter(this.getPosition());
-          alert(this.marker_id);
+          // Gmaps.map.serviceObject.setCenter(this.getPosition());
+          var id = this.marker_id
+
+          if($('.directions').is(':visible')) {
+            transition($('.directions'), $(".community-group-wrapper[data-group-id='" + id + "']"));
+          } else {
+            var elem = $('.community-group-wrapper:visible').first();
+            transition(elem, $(".community-group-wrapper[data-group-id='" + id + "']"));
+          }
         });
       }
     });
   };
 });
+
+function transition(selector_out, selector_in) {
+  selector_out.slideUp('medium', function() {
+    selector_in.slideDown('medium')
+  });
+}

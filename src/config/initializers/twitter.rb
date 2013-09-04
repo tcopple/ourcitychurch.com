a350356a1f9715cec10c5f040ff729ac9af0aaf3
Tyler Copple <j.tylercopple@gmail.com>
2013-09-03 23:46:16 -0400

    Adding twitter gem in place of javascript ajax request.

---
+++ src/config/initializers/twitter.rb
@@ -0,0 +1,8 @@
+require 'twitter'
+
+Twitter.configure do |configure|
+  configure.consumer_key = "vfvRouysBEHj27Vqz9K6Q"
+  configure.consumer_secret = "bUG4DSZ3hMmXWUTY3ZIMHWLrvC38bDFA5iMfzSyw"
+  configure.oauth_token = "250752722-jVo9RH8Y6IMSWE6N81gZPIkhRy4HxnMx2DaTFgLj"
+  configure.oauth_token_secret = "XyIkwti5SST7zHa0tkuFYvvq9rmD68Pj9BIqOgzc"
+end

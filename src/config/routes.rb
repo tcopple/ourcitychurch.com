e761dc2598b3dd2fa583cdd96cf29192e35c749f
Tyler Copple <j.tylercopple@gmail.com>
2013-09-21 17:15:12 -0400

    Removing comments.

--- src/config/routes.rb
+++ src/config/routes.rb
@@ -17,7 +17,5 @@ Citychurch::Application.routes.draw do
   match 'resources' => 'resource#index', :as => :resources
   match ':permalink' => 'pages#show', :as => :permalink
 
-  # You can have the root of your site routed with "root"
-  # just remember to delete public/index.html.
   root :to => 'pages#show', :permalink => 'home'
 end

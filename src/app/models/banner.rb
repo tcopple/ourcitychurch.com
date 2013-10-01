6860258014f068b4f9a04bac8ad060010f399686
Tyler Copple <j.tylercopple@gmail.com>
2013-09-30 21:46:32 -0400

    Removing extraneous pp calls.

--- src/app/models/banner.rb
+++ src/app/models/banner.rb
@@ -9,14 +9,9 @@ class Banner
   field :end_on, type: Time
 
   def live?
-    pp self.file.url
-    pp "#{self.start_on} #{self.end_on}"
-    pp DateTime.now
-
     start = start_on || Time.now - 1
     finish  = end_on || Time.now + 1
-    pp (Time.now > start)
-    pp (Time.now < finish )
+
     return (Time.now > start) && (Time.now < finish )
   end
 end
